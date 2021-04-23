a5430564c8ed94720bffe37ccc57c07a397d044e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-04-23 08:11:23 -0700

    fix: use export default in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-import { join } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-export const mode = "development";
-export const entry = {
-  main: "./src/index.js",
-};
-export const devServer = {
-  contentBase: join(fileURLToPath(import.meta.url), "public"),
-  compress: true,
-  port: 9000,
-  open: true,
-  hot: true,
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  devServer: {
+    contentBase: join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+  },
 };
