e7134a10a4335e14cfc6039e8073792e7e12c3d6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-04-22 21:40:29 -0700

    chore: add example from benchmark

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import benchmark from "benchmark";
+
+const suite = new benchmark.Suite();
+
+suite
+  .add("RegExp#test", function () {
+    /o/.test("Hello World!");
+  })
+  .add("String#indexOf", function () {
+    "Hello World!".indexOf("o") > -1;
+  })
+  .on("cycle", function (event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function () {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  // run async
+  .run({ async: true });
