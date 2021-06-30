464d424be682c9afb7e9cc5c5450ae86e2a1f8a4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-06-30 14:04:07 -0700

    feat: add @aws-crypto/crc32 to CRC32 benchmarks (#4)

--- src/crc32.js
+++ src/crc32.js
@@ -2,6 +2,7 @@ import benchmark from "benchmark";
 import bufferCrc32 from "buffer-crc32";
 import crc from "crc";
 import crc32 from "crc-32";
+import { Crc32 as awsCryptoCrc32 } from "@aws-crypto/crc32";
 
 if (typeof window !== "undefined") {
   // Fix to run benchmark in browser
@@ -23,7 +24,14 @@ console.log(`CRC32 values returned for random buffer:`);
 console.log(`* crc: ${crc.crc32(testBuffer).toString(16)}`);
 console.log(`* crc-32: ${(crc32.buf(testBuffer) >>> 0).toString(16)}`);
 console.log(`* buffer-crc32: ${bufferCrc32.unsigned(testBuffer).toString(16)}`);
+console.log(
+  `* @aws-crypto/crc32: ${new awsCryptoCrc32()
+    .update(testBuffer)
+    .digest(16)
+    .toString(16)}`
+);
 
+const awsCryptoCrc32Obj = new awsCryptoCrc32();
 console.log(`\nBenchmark:`);
 suite
   .add("crc", () => {
@@ -35,11 +43,14 @@ suite
   .add("buffer-crc32", () => {
     bufferCrc32.unsigned(testBuffer).toString(16);
   })
+  .add("@aws-crypto/crc32", () => {
+    awsCryptoCrc32Obj.update(testBuffer).digest(16).toString(16);
+  })
   .on("cycle", (event) => {
     console.log(String(event.target));
   })
   .on("complete", () => {
     console.log("Fastest is " + suite.filter("fastest").map("name"));
   })
-  // run async
+  // run sync
   .run({ async: false });
